ad76d57c8ae8e952a9ef422ca5fc1f5afe133928
Jayce <haoranwang2000@hotmail.com>
2020-06-26 09:44:41 -0400

    Update vgg.py

--- unpackaged/models/vgg.py
+++ unpackaged/models/vgg.py
@@ -25,15 +25,15 @@ VGG11/13/16/19 in Pytorch.
 """
 import torch
 import torch.nn as nn
-
+from torch.nn.modules.module import _addindent
 
 cfg = {
     'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
     'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
     #'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
     #'VGG16': [52, 54, 'M', 106, 106, 'M', 212, 210, 210, 'M', 420, 420, 416, 'M', 414, 414, 414, 'M'], #20%
-    'VGG16': [42, 44, 'M', 84, 82, 'M', 164, 164, 164, 'M', 322, 314, 314, 'M', 312, 314, 318, 'M'], #40%
-    #'VGG16': [32, 36, 'M', 64, 60, 'M', 118, 118, 118, 'M', 226, 214, 216, 'M', 212, 214, 220, 'M'], #60%
+    #'VGG16': [42, 44, 'M', 84, 82, 'M', 164, 164, 164, 'M', 322, 314, 314, 'M', 312, 314, 318, 'M'], #40%
+    'VGG16': [32, 36, 'M', 64, 60, 'M', 118, 118, 118, 'M', 226, 214, 216, 'M', 212, 214, 220, 'M'], #60%
     #'VGG16': [32, 8, 'M', 42, 38, 'M', 72, 72, 72, 'M', 132, 114, 116, 'M', 112, 114, 122, 'M'], #80%
     #'VGG16': [32, 16, 'M', 20, 16, 'M', 28, 26, 26, 'M', 36, 16, 18, 'M', 12, 16, 24, 'M'], #100%
     'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
@@ -44,7 +44,7 @@ class VGG(nn.Module):
     def __init__(self, vgg_name, num_classes: int = 10):
         super(VGG, self).__init__()
         self.features = self._make_layers(cfg[vgg_name])
-        self.classifier = nn.Linear(318, num_classes)
+        self.classifier = nn.Linear(220, num_classes)
 
     def forward(self, x):
         out = self.features(x)
@@ -65,7 +65,7 @@ class VGG(nn.Module):
                 in_channels = x
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
         return nn.Sequential(*layers)
-        
+
 def torch_summarize(model, show_weights=True, show_parameters=True):
     """Summarizes torch model by showing trainable parameters and weights."""
     tmpstr = model.__class__.__name__ + ' (\n'
