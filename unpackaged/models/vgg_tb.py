b6ba9b8533365cdde1c16b053b20e5b10f3cecfe
Jayce <haoranwang2000@hotmail.com>
2020-06-26 16:29:40 -0400

    Create vgg_tb.py

---
+++ unpackaged/models/vgg_tb.py
@@ -0,0 +1,109 @@
+"""
+MIT License
+
+Copyright (c) 2017 liukuang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+VGG11/13/16/19 in Pytorch.
+"""
+import torch
+import torch.nn as nn
+from torch.nn.modules.module import _addindent
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    #'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    #'VGG16': [52, 54, 'M', 106, 106, 'M', 212, 210, 210, 'M', 420, 420, 416, 'M', 414, 414, 414, 'M'], #20%
+    #'VGG16': [42, 44, 'M', 84, 82, 'M', 164, 164, 164, 'M', 322, 314, 314, 'M', 312, 314, 318, 'M'], #40%
+    'VGG16': [32, 36, 'M', 64, 60, 'M', 118, 118, 118, 'M', 226, 214, 216, 'M', 212, 214, 220, 'M'], #60%
+    #'VGG16': [32, 8, 'M', 42, 38, 'M', 72, 72, 72, 'M', 132, 114, 116, 'M', 112, 114, 122, 'M'], #80%
+    #'VGG16': [32, 16, 'M', 20, 16, 'M', 28, 26, 26, 'M', 36, 16, 18, 'M', 12, 16, 24, 'M'], #100%
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name, num_classes: int = 10):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(220, num_classes)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+
+def torch_summarize(model, show_weights=True, show_parameters=True):
+    """Summarizes torch model by showing trainable parameters and weights."""
+    tmpstr = model.__class__.__name__ + ' (\n'
+    for key, module in model._modules.items():
+        # if it contains layers let call it recursively to get params and weights
+        if type(module) in [
+            torch.nn.modules.container.Container,
+            torch.nn.modules.container.Sequential
+        ]:
+            modstr = torch_summarize(module)
+        else:
+            modstr = module.__repr__()
+        modstr = _addindent(modstr, 2)
+
+        params = sum([np.prod(p.size()) for p in module.parameters()])
+        weights = tuple([tuple(p.size()) for p in module.parameters()])
+
+        tmpstr += '  (' + key + '): ' + modstr
+        if show_weights:
+            tmpstr += ', weights={}'.format(weights)
+        if show_parameters:
+            tmpstr +=  ', parameters={}'.format(params)
+        tmpstr += '\n'
+
+    tmpstr = tmpstr + ')'
+    return tmpstr
+
+def test():
+    net = VGG('VGG16')
+    print(torch_summarize(net))
+    tb = SummaryWriter()
+    x = torch.randn(2, 3, 32, 32)
+    tb.add_graph(net,x)
+    tb.close()
+
+    y = net(x)
+    print(y.size())
+
+
+test()
