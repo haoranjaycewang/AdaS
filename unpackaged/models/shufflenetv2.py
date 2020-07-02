cda0f824a9f11aa3aecbb48618f0fafd1a3bf3a5
Jayce <haoranwang2000@hotmail.com>
2020-07-02 11:59:41 -0400

    Update shufflenetv2.py

--- unpackaged/models/shufflenetv2.py
+++ unpackaged/models/shufflenetv2.py
@@ -28,7 +28,8 @@ See the paper "ShuffleNet V2: Practical Guidelines for Efficient CNN Architectur
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from torch.nn.modules.module import _addindent
+import numpy as np
 from ptflops import get_model_complexity_info
 
 class ShuffleBlock(nn.Module):
@@ -175,9 +176,36 @@ configs = {
     }
 }
 
+def torch_summarize(model, show_weights=True, show_parameters=True):
+    """Summarizes torch model by showing trainable parameters and weights."""
+    tmpstr = model.__class__.__name__ + ' (\n'
+    for key, module in model._modules.items():
+        # if it contains layers let call it recursively to get params and weights
+        if type(module) in [
+            torch.nn.modules.container.Container,
+            torch.nn.modules.container.Sequential
+        ]:
+            modstr = torch_summarize(module)
+        else:
+            modstr = module.__repr__()
+        modstr = _addindent(modstr, 2)
+
+        params = sum([np.prod(p.size()) for p in module.parameters()])
+        weights = tuple([tuple(p.size()) for p in module.parameters()])
+
+        tmpstr += '  (' + key + '): ' + modstr
+        if show_weights:
+            tmpstr += ', weights={}'.format(weights)
+        if show_parameters:
+            tmpstr +=  ', parameters={}'.format(params)
+        tmpstr += '\n'
+
+    tmpstr = tmpstr + ')'
+    return tmpstr
 
 def test():
     net = ShuffleNetV2(net_size=0.5)
+    print(torch_summarize(net))
     x = torch.randn(3, 3, 32, 32)
     y = net(x)
 
